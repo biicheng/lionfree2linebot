739936a2ddb608252e6127e2d94b09800925c153
nickH <fish970628@gmail.com>
2021-03-18 18:32:25 +0800

    make it better

--- app/Http/Controllers/LineBotT.php
+++ app/Http/Controllers/LineBotT.php
@@ -92,11 +92,7 @@ class LineBotT extends Controller
         $msg = "text: ".$message_text;//信件內容
         $headers = "k1031616@yahoo.com.tw"; //寄件者
         
-        if(mail("$to", "$subject", "$msg", "$headers")){
-            echo "信件已經發送成功。";//寄信成功就會顯示的提示訊息
-        }else{
-            echo "信件發送失敗！";//寄信失敗顯示的錯誤訊息
-        }
+        mail($to, $subject, $msg, $headers);
         
         // \Log::info($response->getHTTPStatus() . ' ' . $response->getRawBody());
         // \Log::info('1message_text: '.$message_text);
