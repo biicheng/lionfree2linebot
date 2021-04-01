9e124aad2005c9cecea579fa06f578814b80632c
nickH <fish970628@gmail.com>
2021-04-01 17:06:00 +0800

    make it better

--- app/Http/Controllers/LineBotT.php
+++ app/Http/Controllers/LineBotT.php
@@ -102,12 +102,12 @@ class LineBotT extends Controller
         $messages = DB::select('select * from message where u_text=?', [$message_text]);
         // \Log::info(count($messages));
         if(count($messages)==1){
-            if($mm->reType=='text'){
-                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($messages->re_text);
+            if($mm['reType']=='text'){
+                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($messages['re_text']);
                 $response = $bot->replyMessage($replyToken, $textMessageBuilder);
             }
             else{
-                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($mm->re_text);
+                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($messages['re_text']);
                 $response = $bot->replyMessage($replyToken, $textMessageBuilder);
             }
         }
