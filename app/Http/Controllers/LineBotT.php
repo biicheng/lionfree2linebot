0c1ca85f64d81516789798794257e4597695715d
nickH <fish970628@gmail.com>
2021-04-12 13:12:14 +0800

    make it better

--- app/Http/Controllers/LineBotT.php
+++ app/Http/Controllers/LineBotT.php
@@ -22,8 +22,9 @@ class LineBotT extends Controller
     private $sender;
     public function postTest(Request $cc){
         $dates = date("Y-m-d H:i:s");
-        
-        if(!empty($cc->input('events')[0]['message']['text'])){
+        if((!empty($cc->input('events')[0]['message']['text']))||
+            ($cc->input('events')[0]['message']['type']=='text')){
+        // if(!empty($cc->input('events')[0]['message']['text'])){
             $destination = $cc->input('destination');
             $events_type = $cc->input('events')[0]['type'];
             $replyToken = $cc->input('events')[0]['replyToken'];
@@ -37,116 +38,68 @@ class LineBotT extends Controller
             $source_userId = $cc->input('events')[0]['source']['userId'];
             $source_type = $cc->input('events')[0]['source']['type'];
     
-            // if($source_userId!='U7b6cf61ae9975bc4b800b1146a840ed2'){
-            //     DB::insert('insert into lined (datT, dataTime) values (?, ?)', [$obj, $dates]);
-            // }
-            
-        
-    
-            // $ch = curl_init();
-            // $url = "https://api.line.me/v2/bot/profile/".$source_userId;
-            // $ch = curl_init();
-            // curl_setopt($ch, CURLOPT_URL, $url);
-            // curl_setopt($ch, CURLOPT_POST, 0);
-            // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            // $response = curl_exec ($ch);
-            // $err = curl_error($ch);  //if you need
-            // curl_close ($ch);
-    
+
             // $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('ym0T5CEd4bHEZMZiGPalBWAS/YgXNznsTAmI5v83bMHRIEdxA6MyQ7B7KG0jRPgfjitgebHz9PL0IaJym/7IrhoaPyOF+6gDTjuKB6mN+FuYncPrcW95Fe2vJKqskTWkfu3vVTV4GPWIyVNW3ZdGSgdB04t89/1O/w1cDnyilFU=');
             // $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '4b91553e4c688509a050ba0f29208a90']);
-            // $response = $bot->getProfile($source_userId);
-            // if ($response->isSucceeded()) {
-            //     $profile = $response->getJSONDecodedBody();
-            //     if(!empty($profile['displayName'])){
-            //         \Log::info('displayName: '.$profile['displayName']);
-            //     }
-            //     else{
-            //         \Log::info('displayName: 無'.', err:'.$err);
-            //     }
-            //     if(!empty($profile['pictureUrl'])){
-            //         \Log::info('pictureUrl: '.$profile['pictureUrl']);
-            //     }
-            //     else{
-            //         \Log::info('pictureUrl: 無'.', err:'.$err);
-            //     }
-            //     if(!empty($profile['statusMessage'])){
-            //         \Log::info('statusMessage: '.$profile['statusMessage']);
-            //     }
-            //     else{
-            //         \Log::info('statusMessage: 無'.', err:'.$err);
-            //     }
-            // }
-            
-            $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('ym0T5CEd4bHEZMZiGPalBWAS/YgXNznsTAmI5v83bMHRIEdxA6MyQ7B7KG0jRPgfjitgebHz9PL0IaJym/7IrhoaPyOF+6gDTjuKB6mN+FuYncPrcW95Fe2vJKqskTWkfu3vVTV4GPWIyVNW3ZdGSgdB04t89/1O/w1cDnyilFU=');
-            $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '4b91553e4c688509a050ba0f29208a90']);
     
             /* sele遠方mysql */
-            $i = 0;
-            $messages = DB::select('select * from message where u_text=?', [$message_text]);
-            if(count($messages)==1){
-                if($messages[0]->reType=='text'){
-                    $txt = $this->pushText($messages[0]->re_text, $replyToken);
-                }
-                else if($messages[0]->reType=='select'){
-                }
-                else if($messages[0]->reType=='img'){
-                    $txt = $this->pushImg($messages[0]->bImg, $messages[0]->sImg, $replyToken);
-                }
-                else{
-                    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($messages[0]->re_text);
-                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
-                }
-            }
-            else{
-                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message_text);
-                $response = $bot->replyMessage($replyToken, $textMessageBuilder);
-            }
-            
-            /* curl 000webhost API */
-            // $ch = curl_init();
-            // //curl_setopt可以設定curl參數
-            // //設定url
-            // curl_setopt($ch , CURLOPT_URL , "https://tkogo.000webhostapp.com/botController/".$message_text);
-
-            // //獲取結果不顯示
-            // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-            // //設定AGENT
-            // curl_setopt($ch, CURLOPT_USERAGENT, "Google Bot");
-            // //執行，並將結果存回
-            // $result = curl_exec($ch);
-            // //關閉連線
-            // curl_close($ch);
-
-            // $reD = json_decode($result);
-            // if($reD->reType=='text'){
-            //     $txt = $this->pushText($reD->re_text, $replyToken);
-            // }
-            // else if($reD->reType=='img'){
-            //     $txt = $this->pushImg($reD->bImg, $reD->sImg, $replyToken);
+            // $i = 0;
+            // $messages = DB::select('select * from message where u_text=?', [$message_text]);
+            // if(count($messages)==1){
+            //     if($messages[0]->reType=='text'){
+            //         $txt = $this->pushText($messages[0]->re_text, $replyToken);
+            //     }
+            //     else if($messages[0]->reType=='select'){
+            //         $txt = $this->pushText($message_text, $replyToken);
+            //     }
+            //     else if($messages[0]->reType=='img'){
+            //         $txt = $this->pushImg($messages[0]->bImg, $messages[0]->sImg, $replyToken);
+            //     }
+            //     else{
+            //         $txt = $this->pushText($message_text, $replyToken);
+            //         // $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($messages[0]->re_text);
+            //         // $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+            //     }
             // }
             // else{
             //     $txt = $this->pushText($message_text, $replyToken);
+            //     // $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message_text);
+            //     // $response = $bot->replyMessage($replyToken, $textMessageBuilder);
             // }
             
-            if ($response->isSucceeded()) {
-                \Log::info($source_userId.' --bot: yes.');
+            /* curl 000webhost API */
+            $ch = curl_init();
+            //curl_setopt可以設定curl參數
+            //設定url
+            curl_setopt($ch , CURLOPT_URL , "https://tkogo.000webhostapp.com/botController/".$message_text);
+
+            //獲取結果不顯示
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            //設定AGENT
+            curl_setopt($ch, CURLOPT_USERAGENT, "Google Bot");
+            //執行，並將結果存回
+            $result = curl_exec($ch);
+            //關閉連線
+            curl_close($ch);
+
+            $reD = json_decode($result);
+            if($reD->reType=='text'){
+                $txt = $this->pushText($reD->re_text, $replyToken);
+            }
+            else if($reD->reType=='img'){
+                $txt = $this->pushImg($reD->bImg, $reD->sImg, $replyToken);
             }
             else{
-                \Log::info($source_userId.' --bot: no.');
+                $txt = $this->pushText($message_text, $replyToken);
             }
         }
         else{
-            $txt = $this->pushImg('https://tkolifego.000webhostapp.com/img/linebot_img/you-say-chineseB.jpg', 'https://tkolifego.000webhostapp.com/img/linebot_img/you-say-chineseS.jpg', $replyToken);
+            $txt = $this->pushImg('https://tkolifego.000webhostapp.com/img/linebot_img/you-say-chineseB.jpg', 'https://tkolifego.000webhostapp.com/img/linebot_img/you-say-chineseS.jpg', $cc->input('events')[0]['replyToken']);
             // $txt = $this->pushText('請輸入文字...', $cc->input('events')[0]['replyToken']);
         }
         
-        $this->lineUserData($source_userId);
-        // \Log::info($response->getHTTPStatus() . ' ' . $response->getRawBody());
-        // \Log::info('1message_text: '.$message_text);
-        // \Log::info($replyToken);
-
+        $this->lineUserData($cc->input('events')[0]['source']['userId']);
         return 'hello.';
     }
 
@@ -156,6 +109,12 @@ class LineBotT extends Controller
 
         $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($bImg, $sImg);
         $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+        if ($response->isSucceeded()) {
+            \Log::info(' --bot_img: yes.');
+        }
+        else{
+            \Log::info(' --bot_img: no.');
+        }
         return 'ok';
     }
     public function pushText($text, $replyToken){
@@ -164,6 +123,12 @@ class LineBotT extends Controller
 
         $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
         $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+        if ($response->isSucceeded()) {
+            \Log::info(' --bot_text: yes.');
+        }
+        else{
+            \Log::info(' --bot_text: no.');
+        }
         return 'ok';
     }
     public function lineUserData($source_userId){
