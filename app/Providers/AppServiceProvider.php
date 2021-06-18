00bcb514f41605510eb698161333d5c8fe3c8112
nickH <fish970628@gmail.com>
2021-06-18 12:05:58 +0800

    updateT6184

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        // if($this->app->environment('production')){
-        //     URL::forceScheme('https');
-        // }
+        if($this->app->environment('production')){
+            URL::forceScheme('https');
+        }
     }
 }
