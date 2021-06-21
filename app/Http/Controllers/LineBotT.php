84159cd80fcfddffc0ef25e23296ef2691ca3615
nickH <fish970628@gmail.com>
2021-06-21 15:21:43 +0800

    updateT62115

--- app/Http/Controllers/LineBotT.php
+++ app/Http/Controllers/LineBotT.php
@@ -279,8 +279,12 @@ class LineBotT extends Controller
         $row = null;
         // $messageD = DB::select('SELECT * from sql6401619.message where u_text="抽菸的規矩"');
         try {
-            $posts=DB::table('sql6401619.message')->where('u_text', '=', "tw")->count();
-            return 'select date count: '.$posts.'筆';
+            // $posts=DB::table('sql6401619.message')->where('u_text', '=', "tw")->count();
+            // return 'select date count: '.$posts.'筆';
+            $posts=DB::table('sql6401619.message')->get();
+            foreach($posts as $v){
+                return$v->reType.'<br>';
+            }
         } catch (\Exception $exception) {
             dd($exception->getMessage());//注意不要輸出這個
         }
