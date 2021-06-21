735b2e9fb90575ae71b0223d04c5d688a7014007
nickH <fish970628@gmail.com>
2021-06-21 19:36:45 +0800

    updateT62159

--- app/Http/Controllers/LineBotT.php
+++ app/Http/Controllers/LineBotT.php
@@ -49,7 +49,7 @@ class LineBotT extends Controller
             try{
                 $sql = DB::table('sql6401619.message')->where('u_text', '=',$message_text,'oc','=',1)->get();
                 if(!empty($sql[0])){
-                    // \Log::info(' --db: '.json_encode($sql).'---');
+                    \Log::info(' --db: '.json_encode($sql).'---');
                     if($sql[0]->reType=='text'){
                         $txt = $this->pushText($sql[0]->re_text, $replyToken);
                     } 
