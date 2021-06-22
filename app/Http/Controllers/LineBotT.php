2436e257cde1ba2f6010496f0d02388a4defdda5
nickH <fish970628@gmail.com>
2021-06-22 12:34:46 +0800

    updateT6223

--- app/Http/Controllers/LineBotT.php
+++ app/Http/Controllers/LineBotT.php
@@ -340,8 +340,15 @@ class LineBotT extends Controller
     
     public function dbT()
     {
-        $row = null;
-        // $messageD = DB::select('SELECT * from sql6401619.message where u_text="抽菸的規矩"');
+        $u_text = 'tw';
+        $oc = 0;
+        $sqls = DB::table('sql6401619.message')->where('u_text','=',$u_text)->get();
+        if($sqls[0]->oc==0){
+            $oc = 1;
+        }
+        else{
+            $oc = 0;
+        }
         try {
             $ttt = null;
             // $posts=DB::table('sql6401619.message')->where('u_text', '=', "tw")->count();
@@ -353,15 +360,6 @@ class LineBotT extends Controller
             // }
             // return $ttt;
 
-            $u_text = 'tw';
-            $oc = 0;
-            $sqls = DB::table('sql6401619.message')->where('u_text','=',$u_text)->get();
-            if($sqls[0]->oc==0){
-                $oc = 1;
-            }
-            else{
-                $oc = 0;
-            }
             // $uodateData = DB::table('sql6401619.message')->where('u_text','=',$u_text)->update('oc','=',0);
             $uodateData = DB::update('update sql6401619.message set oc=? where u_text=?', [$oc, '"'.$u_text.'"']);
             $sqlss = DB::table('sql6401619.message')->where('u_text','=',$u_text)->get();
