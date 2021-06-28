bec0c5fddb40e5af7f94bb957b288fd37ba33743
nickH <fish970628@gmail.com>
2021-06-28 11:41:20 +0800

    updateT6282

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -37,9 +37,9 @@ class Controller extends BaseController
             '/', $days.'.'.$imgType[1]
         );
         \Log::info(' --path--'.$path.'--'.$days.'.'.$imgType[1]);//./img/LineBot_Img
-        File::copy('/app/storage/app/'.$path, '/public/img/LineBot_Img/'.$path);
+        // File::copy('/app/storage/app/'.$path, './img/LineBot_Img'.$path);
         // File::copy(storage_path().'\\app\\'.$path, './images/'.$path);
-        Storage::delete('./'.$path);
+        // Storage::delete('./'.$path);
         return $path;
     }
 }
