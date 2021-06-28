b59899d7b1b4c38a53eda465a72f7f7fb87ba9c1
nickH <fish970628@gmail.com>
2021-06-28 11:54:50 +0800

    updateT6282

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -36,8 +36,9 @@ class Controller extends BaseController
         $path = $request->file('imgs')->storeAs(
             '/', $days.'.'.$imgType[1]
         );
-        \Log::info(' --path--'.$path.'--'.$days.'.'.$imgType[1]);
-        File::copy(storage_path().'\\app\\'.$path, './images/'.$path);
+        \Log::info(' --path--'.$path.'--'.$days.'.'.$imgType[1]);//./img/LineBot_Img
+        File::copy('/app/storage/app/'.$path, './img/LineBot_Img'.$path);
+        // File::copy(storage_path().'\\app\\'.$path, './images/'.$path);
         Storage::delete('./'.$path);
         return $path;
     }
