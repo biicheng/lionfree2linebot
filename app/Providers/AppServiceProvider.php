337044e59663de00987d374c31c8b872b093a085
nick <k1031616@yahoo.com.tw>
2022-05-13 13:31:01 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 
+use Illuminate\Support\Facades\App;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -28,5 +30,8 @@ class AppServiceProvider extends ServiceProvider
         // if(!$this->app->environment('production')){
         //     URL::forceScheme('https');
         // }
+        if (App::isProduction()) {
+            URL::forceScheme('https');
+        }
     }
 }
