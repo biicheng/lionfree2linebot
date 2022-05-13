48bc371ba398ce38ff8e1b841e52707ed667a337
nick <k1031616@yahoo.com.tw>
2022-05-13 12:27:01 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,6 +22,7 @@
                     </a>
                 </div>
             </div>
+            <img src="{{ url('storage/app/public/GJ4Tm6j7VbDImdAGxAXd9qJOQLoTyO5nGVTekfDf.png') }}" alt="" title="" />
                 {{-- <a href="{{route('update')}}" style="text-decoration:none;">
                     <div class="alert alert-warning" style="margin-top:5%;border:1px #A9A9A9 solid;">
                         更新關鍵詞語.
