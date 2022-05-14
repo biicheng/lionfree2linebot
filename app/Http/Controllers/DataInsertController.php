899f26db246c26a19b4614cd59a8708253be6961
nick <k1031616@yahoo.com.tw>
2022-05-14 11:04:03 +0800

    update DataInsertController

--- app/Http/Controllers/DataInsertController.php
+++ app/Http/Controllers/DataInsertController.php
@@ -50,52 +50,8 @@ class DataInsertController extends Controller
 
     public function insertD(Request $request)
     {
-        // $request = $request->all();
-        // if($request['reType']=='text' && $request['BotText']!=""){
-        //     return view('insert',[
-        //         'alert'=>'success',
-        //         'alertT'=>'新增文字回覆成功.',
-        //         'postT'=>true,
-        //     ]);
-        // }
-        // else if($request['reType']=='img' && $request['bImg']!="" && $request['sImg']!=""){
-        //     return view('insert',[
-        //         'alert'=>'success',
-        //         'alertT'=>'新增圖片回覆成功.',
-        //         'postT'=>true,
-        //     ]);
-        // }
-        // else{//insertErr
-        //     return view('/insert',[
-        //         'alert'=>'danger',
-        //         'alertT'=>'資訊錯誤，新增失敗.',
-        //         'postT'=>true,
-        //     ]);
-        // }
-        #正常可以用
-        // Storage::disk('local')->put('file.txt', 'Contents');
-
-        // $request = $request->input();
-        // $request = $request->all();
-
         $bImgName = Storage::disk('updateImg')->put('img/'.$request->input('bImg'), $request->file('bImg'), 'public');
-        // Storage::putFileAs('photos', new File(), $request->file('bImg'));
-        // $bImgNewName = explode('//', $bImgName);
         $bImgNewName = mb_split('//', $bImgName);
-        // Storage::move($bImgNewName[1], public_path().$bImgNewName[1]);
-        // $bImgURL = 'public/'.Storage::url($bImgNewName[1]);
-        // Storage::copy(storage_path('app\\'.$bImgNewName[1]), public_path('img\\').$bImgNewName[1]);
-        // $bImgNewName->copy(storage_path('app\public'));
-
-        // $sImgName = Storage::disk('local')->put('public/'.$request->input('bImg'),$request->file('sImg'));
-        // $sImgNewName = explode('//', $sImgName);
-        // $sImgNewName = mb_split('//', $sImgName);
-
-        //檢查檔案是否存在
-        // Storage::exists('old/file.jpg')
-
-        \Log::info("...".Storage::exists(storage_path('app\\'.$bImgNewName[1])));
-        // echo storage_path('app\public\\').$bImgNewName[1];
         if($request->input('reType')=='text' && $request->input('BotText')!=""){
             return view('insert',[
                 'alert'=>'success',
