b83c479866f7323ab672436b533991090671bc74
nick <k1031616@yahoo.com.tw>
2022-07-10 11:08:03 +0800

    update

--- app/Http/Controllers/DB/dataInsertController.php
+++ app/Http/Controllers/DB/dataInsertController.php
@@ -24,7 +24,7 @@ class dataInsertController extends Controller
     public function botMessage_Img_Insert($reType, $u_text, $bImg, $sImg){
         try{
             DB::table('botmessage2line')->insert([
-                'mIndex'=>null, 'reType'=>$reType, 'u_text'=>$u_text, 
+                'mIndex'=>0, 'reType'=>$reType, 'u_text'=>$u_text, 
                 'bImg'=>$bImg, 'sImg'=>$sImg, 'oc'=>1
             ]);
             return 1;
